dab8ee924ddb176d7200daecd37440c4c6e436b2
shahriyardx <mdshahriyaralam552@gmail.com>
2022-11-27 04:17:09 +0600

    handle error while uploading

--- src/routers/upload.ts
+++ src/routers/upload.ts
@@ -9,6 +9,7 @@ import { type Request, type Response } from "express"
 import prisma from "../db/client"
 import validateToken from "../middlewares/validateToken"
 import { getSafeFileName } from "../utils"
+import { Prisma } from "@prisma/client"
 
 const router = Router()
 
@@ -40,16 +41,30 @@ router.post(
     if (!fs.existsSync(destination)) fs.mkdirSync(destination)
 
     const final_destination = path.join(destination, fileName + extension)
-    fs.writeFileSync(final_destination, image.buffer)
+    try {
+      fs.writeFileSync(final_destination, image.buffer)
+    } catch {
+      return res.status(500).json({ message: "Error creating file" })
+    }
 
-    await prisma.image.create({
-      data: {
-        folder: body.folder || "default",
-        filename: fileName,
-        extension,
-        filesize: image.size,
-      },
-    })
+    try {
+      await prisma.image.create({
+        data: {
+          folder: body.folder || "default",
+          filename: fileName,
+          extension,
+          filesize: image.size,
+        },
+      })
+    } catch (err) { 
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === "P2002") {
+          return res.status(400).json({ message: "File already exists" })
+        } else {
+          return res.status(500).json({ message: "Internal server error" })
+        }
+      }
+    }
 
     res.json({
       success: true,
